7bdd6a3e3858af1b1d95ee5daab32d6886bc051c
Lsunae <sjsao5012@gmail.com>
2022-06-27 17:25:22 +0900

    패키지 생성 및 위치 변경, 베이스 액티비티 적용

--- app/src/main/java/com/leesunae/bebehelper_mvp/base/BaseActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.leesunae.bebehelper_mvp.base
-
-class BaseActivity {
-}
--- app/src/main/java/com/leesunae/bebehelper_mvp/view/MainActivity.kt
+++ app/src/main/java/com/leesunae/bebehelper_mvp/view/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.leesunae.bebehelper_mvp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.leesunae.bebehelper_mvp.R
+import com.leesunae.bebehelper_mvp.view.base.BaseActivity
+import com.leesunae.bebehelper_mvp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
     }
 }
