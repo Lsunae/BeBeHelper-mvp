535d9df212a7ee182bc94886e58cb01d818334a8
Lsunae <sjsao5012@gmail.com>
2022-07-01 17:40:27 +0900

    user db 코드 작성

--- app/src/main/java/com/leesunae/bebehelper_mvp/data/room/database/UserDB.kt
+++ app/src/main/java/com/leesunae/bebehelper_mvp/data/room/database/UserDB.kt
@@ -5,10 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.leesunae.bebehelper_mvp.data.room.dao.UserDao
-import com.leesunae.bebehelper_mvp.data.room.entity.UserEntity
+import com.leesunae.bebehelper_mvp.data.room.entity.User
 
 
-@Database(entities = [UserEntity::class], version = 1)
+@Database(entities = [User::class], version = 1)
 abstract class UserDB : RoomDatabase() {
     abstract fun userDao(): UserDao?
 
