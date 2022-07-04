6abd130ca17b295ced39094a9dd35fd97eca3b57
Lsunae <sjsao5012@gmail.com>
2022-07-04 19:05:27 +0900

    회원가입 클래스 및 레이아웃 화면 생성 및 작업중

---
+++ app/src/main/java/com/leesunae/bebehelper_mvp/view/sign/SignUpActivity.kt
@@ -0,0 +1,19 @@
+package com.leesunae.bebehelper_mvp.view.sign
+
+import android.os.Bundle
+import com.leesunae.bebehelper_mvp.R
+import com.leesunae.bebehelper_mvp.databinding.ActivitySignUpBinding
+import com.leesunae.bebehelper_mvp.view.base.BaseActivity
+
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setUpView()
+    }
+
+    private fun setUpView() {
+
+    }
+}
