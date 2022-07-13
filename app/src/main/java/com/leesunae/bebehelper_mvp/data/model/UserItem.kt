e5d51b11b8932637e907b6286263e365230e339a
Lsunae <sjsao5012@gmail.com>
2022-07-13 16:57:14 +0900

    유저 정보 모델 추가

---
+++ app/src/main/java/com/leesunae/bebehelper_mvp/data/model/UserItem.kt
@@ -0,0 +1,18 @@
+package com.leesunae.bebehelper_mvp.data.model
+
+import android.provider.ContactsContract
+
+data class UserItem(
+    var email: String = "",
+    var password: String = "",
+    var passwordConfirm: String = "",
+    var nickname: String = "",
+    var gender: String = "",
+    var childGender: String = "",
+    var ageOfChildren: String = "",
+    var area: String = "",
+    var image: String = "",
+    var isCheckedEmail: Boolean = false,
+    var isCheckedPassword: Boolean = false,
+    var isCheckedNickname: Boolean = false,
+)
