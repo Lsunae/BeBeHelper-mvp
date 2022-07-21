fc7673ebf6768280775cc7ac6937633d47f01f8d
Lsunae <sjsao5012@gmail.com>
2022-07-21 18:21:48 +0900

    그룹핑 글 작성 버튼 추가 및 클릭 이벤트 추가

--- app/src/main/java/com/leesunae/bebehelper_mvp/view/grouping/GroupingFragment.kt
+++ app/src/main/java/com/leesunae/bebehelper_mvp/view/grouping/GroupingFragment.kt
@@ -1,11 +1,13 @@
 package com.leesunae.bebehelper_mvp.view.grouping
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.leesunae.bebehelper_mvp.R
 import com.leesunae.bebehelper_mvp.databinding.FragmentGroupingBinding
+import com.leesunae.bebehelper_mvp.util.OnSingleClickListener
 import com.leesunae.bebehelper_mvp.view.base.BaseFragment
 
 class GroupingFragment : BaseFragment<FragmentGroupingBinding>(R.layout.fragment_grouping) {
@@ -20,10 +22,21 @@ class GroupingFragment : BaseFragment<FragmentGroupingBinding>(R.layout.fragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setClickListener()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+    }
+
+    private fun setClickListener() {
+        binding.apply {
+            ivCreate.setOnClickListener(object : OnSingleClickListener() {
+                override fun onSingleClick(v: View) {
+                    val intent = Intent(requireContext(), GroupingCreateActivity::class.java)
+                    startActivity(intent)
+                }
+            })
+        }
     }
 }
