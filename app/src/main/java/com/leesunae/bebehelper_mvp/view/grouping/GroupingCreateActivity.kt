4cc38299eac1ff47f31494b0bf69c6426acea7b8
Lsunae <sjsao5012@gmail.com>
2022-07-21 18:20:33 +0900

    그룹 생성 화면 구현

---
+++ app/src/main/java/com/leesunae/bebehelper_mvp/view/grouping/GroupingCreateActivity.kt
@@ -0,0 +1,15 @@
+package com.leesunae.bebehelper_mvp.view.grouping
+
+import android.os.Bundle
+import com.leesunae.bebehelper_mvp.R
+import com.leesunae.bebehelper_mvp.databinding.ActivityGroupingCreateBinding
+import com.leesunae.bebehelper_mvp.view.base.BaseActivity
+
+class GroupingCreateActivity :
+    BaseActivity<ActivityGroupingCreateBinding>(R.layout.activity_grouping_create) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
