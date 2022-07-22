edab31c493ff5a261c7e8b260208fe01c2db72e5
Lsunae <sjsao5012@gmail.com>
2022-07-22 17:35:44 +0900

    Injection에 GroupingRepository 추가

--- app/src/main/java/com/leesunae/bebehelper_mvp/Injection.kt
+++ app/src/main/java/com/leesunae/bebehelper_mvp/Injection.kt
@@ -1,9 +1,13 @@
 package com.leesunae.bebehelper_mvp
 
-import com.leesunae.bebehelper_mvp.data.repository.UserRepository
-import com.leesunae.bebehelper_mvp.data.repository.UserRepositoryImpl
+import com.leesunae.bebehelper_mvp.data.repository.grouping.GroupingRepository
+import com.leesunae.bebehelper_mvp.data.repository.grouping.GroupingRepositoryImpl
+import com.leesunae.bebehelper_mvp.data.repository.user.UserRepository
+import com.leesunae.bebehelper_mvp.data.repository.user.UserRepositoryImpl
+import com.leesunae.bebehelper_mvp.data.room.database.GroupingDB
 import com.leesunae.bebehelper_mvp.data.room.database.UserDB
-import com.leesunae.bebehelper_mvp.data.source.local.UserLocalDataSourceImpl
+import com.leesunae.bebehelper_mvp.data.source.local.grouping.GroupingLocalDataSourceImpl
+import com.leesunae.bebehelper_mvp.data.source.local.user.UserLocalDataSourceImpl
 import com.leesunae.bebehelper_mvp.util.AppExecutors
 
 object Injection {
@@ -12,6 +16,15 @@ object Injection {
             UserLocalDataSourceImpl.getInstance(
                 AppExecutors(),
                 UserDB.getInstance(App.instance.context())
+            )
+        )
+    }
+
+    fun groupingRepository(): GroupingRepository {
+        return GroupingRepositoryImpl.getInstance(
+            GroupingLocalDataSourceImpl.getInstance(
+                AppExecutors(),
+                GroupingDB.getInstance(App.instance.context())
             )
         )
     }
